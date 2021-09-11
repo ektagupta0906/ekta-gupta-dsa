f6263a8a63f1a173fdb9729944cf67e246ef4723
ekta gupta <83336435+20100BTCSDSI07270@users.noreply.github.com>
2021-09-11 20:38:20 +0530

    Create dsa1.cpp

---
+++ dsa1.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ char word[10]= {'D','N','A','L','R','E','H','T','O','M'};
+ cout<<"After reversing the order in array."<<endl;
+ for(int i=9;i>=0;i--)
+ {
+ cout<<word[i];
+ }
+ getch();
+ }
