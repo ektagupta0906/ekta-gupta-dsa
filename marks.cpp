d6bc1ceea947449e592b30801e050253ef3e97a4
ekta gupta <83336435+20100BTCSDSI07270@users.noreply.github.com>
2021-12-06 14:52:44 +0530

    marks.cpp

---
+++ marks.cpp
@@ -0,0 +1,18 @@
+//program to show marks of students of five subjects and print subject wise
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float p, c, m, e, h;
+    
+    // p, c, m, e, and h are the five subjects
+    // p = physics
+    // c = chemistry
+    // m = math
+    // e = english
+    // h = history
+
+    cout << "Enter the marks of five subjects::\n";
+    cin >> p >> c >> m >> e >> h;
